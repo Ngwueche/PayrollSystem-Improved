6ef891e424361ece61fb519b1f1c635ace774b4c
ngwueche <wisdomngwueche@gmail.com>
2023-10-29 00:36:39 +0100

    Add project files.

---
+++ Model/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace PayrollSystem.Model
+{
+    public abstract class BaseEntity
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+    }
+}
---
+++ Model/Salary.cs
@@ -0,0 +1,6 @@
+﻿namespace PayrollSystem.Model
+{
+    public class Salary : BaseEntity
+    {
+    }
+}
